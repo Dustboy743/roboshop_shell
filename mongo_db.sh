120b19658e51fd5c9b470c2a6d90e58c73f3d8a3
Dustboy743 <dsaidns@gmail.com>
2025-05-20 19:11:04 +0530

    creating instances

--- mongo_db.sh
+++ mongo_db.sh
@@ -49,6 +49,9 @@ VALIDATION $? "Starting MongoDB"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATION $? "Editing MongoDB conf file for remote connections"
 
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarting MongoDB"
+
 
 
     

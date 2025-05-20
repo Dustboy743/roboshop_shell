1db369b29af6caaa8816393df55670f878aa3b2e
Dustboy743 <dsaidns@gmail.com>
2025-05-20 18:30:48 +0530

    creating instances

--- frontend.sh
+++ frontend.sh
@@ -36,7 +36,7 @@ VALIDATION()
 
 dnf module disable nginx -y &>> $log_name
 dnf module enable nginx:1.24 -y &>> $log_name
-dnf install nginx -y | tee -a $log_name
+dnf install nginx -y | tee -y &>> $log_name
 VALIDATION $? "nginx installation" 
 
 systemctl enable nginx &>> $log_name
@@ -52,7 +52,7 @@ cd /usr/share/nginx/html &>> $log_name
 unzip /tmp/frontend.zip  &>> $log_name
 VALIDATION $? "extraction of html files"
 
-cp $nginx.conf /etc/nginx/nginx.conf &>> $log_name
+cp $current_directory/nginx.conf /etc/nginx/nginx.conf &>> $log_name
 VALIDATION $? "copying of nginx conf files"
 
 systemctl restart nginx 

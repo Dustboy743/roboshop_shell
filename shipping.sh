93a3ab51be6172eda4434aae2c416d4ca98ff4c9
Dustboy743 <dsaidns@gmail.com>
2025-05-22 02:02:12 +0530

    create instance

--- shipping.sh
+++ shipping.sh
@@ -75,12 +75,12 @@ dnf install mysql -y  &>> $log_name
 VALIDATION $? "installing of mysql"
 
 #to check already data is copied
-#mysql -h mysql.jiony.xyz -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$log_name
+#mysql -h mysql.jiony.xyz -uroot -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$log_name
 #if [ &? -ne 0 ]
 #then #loading data
-mysql -h mysql.jiony.xyz -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$log_name
-mysql -h mysql.jiony.xyz -uroot -p$MYSQL_ROOT_PASSWORD  < /app/db/app-user.sql &>>$log_name
-mysql -h mysql.jiony.xyz -uroot -p$MYSQL_ROOT_PASSWORD  < /app/db/master-data.sql &>>$log_name
+mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$log_name
+mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$log_name
+mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$log_name
 VALIDATION $? "loading data into mysql"
 
 systemctl restart shipping &>>$log_name

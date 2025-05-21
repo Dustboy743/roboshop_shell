5ede982dca8396f49deeed2e23fe4e93b6754c3f
Dustboy743 <dsaidns@gmail.com>
2025-05-21 23:47:49 +0530

    instances create

--- redis.sh
+++ redis.sh
@@ -36,15 +36,15 @@ VALIDATION()
 
 dnf module disable redis -y  &>> $log_name
 dnf module enable redis:7 -y &>> $log_name
-$VALIDATION $? "Redis enable" 
+VALIDATION $? "Redis enable" 
 
 dnf install redis -y | tee -a $log_name
-$VALIDATION $? "Redis install" 
+VALIDATION $? "Redis install" 
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>> $log_name
 sed -i 's/protected-mode yes/protected-mode no/g' /etc/redis/redis.conf &>> $log_name
-$VALIDATION $? "edit the conf file"
+VALIDATION $? "edit the conf file"
 
 systemctl enable redis  &>> $log_name
 systemctl start redis  &>> $log_name
-$VALIDATION $? "STARTING OF REDIS"
+VALIDATION $? "STARTING OF REDIS"

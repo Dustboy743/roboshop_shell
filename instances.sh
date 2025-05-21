3c56da46a8a881ac660db076f3447eee56ac4686
Dustboy743 <dsaidns@gmail.com>
2025-05-21 22:47:40 +0530

    creating instances

--- instances.sh
+++ instances.sh
@@ -6,17 +6,16 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z05347282OEORUJ5H4UMD" # replace with your ZONE ID
 DOMAIN_NAME="jiony.xyz" # replace with your domain
 
-#for instance in ${INSTANCES[@]}
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        $RECORD="$instance"."$DOMAIN_NAME"
+        RECORD="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        $RECORD="$DOMAIN_NAME"
+        RECORD="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
 
@@ -28,7 +27,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : '$RECORD'
+            "Name"              : "'$RECORD'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{

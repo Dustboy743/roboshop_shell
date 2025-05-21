1f401f0d23d8f15084ec35b0d4ebad6e2e3dd33b
Dustboy743 <dsaidns@gmail.com>
2025-05-21 17:24:01 +0530

    instances

--- instances.sh
+++ instances.sh
@@ -6,7 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z05347282OEORUJ5H4UMD" # replace with your ZONE ID
 DOMAIN_NAME="jiony.xyz" # replace with your domain
 
-for instance in ${INSTANCES[@]}
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
